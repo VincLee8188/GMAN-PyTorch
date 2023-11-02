e328b4e8bcf51c937c755ab0ec05c666c739a4a8
moghadas76 <s.m.moghadas2012@gmail.com>
2023-11-02 11:21:52 +0000

    Fix dinominator problem

--- utils/utils_.py
+++ utils/utils_.py
@@ -75,7 +75,7 @@ def load_data(args):
     time = pd.DatetimeIndex(df.index)
     dayofweek = torch.reshape(torch.tensor(time.weekday), (-1, 1))
     timeofday = (time.hour * 3600 + time.minute * 60 + time.second) \
-                // time.freq.delta.total_seconds()
+                // (5 * 60)
     timeofday = torch.reshape(torch.tensor(timeofday), (-1, 1))
     time = torch.cat((dayofweek, timeofday), -1)
     # train/val/test
